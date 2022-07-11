44dc4b04f7793bdaa6e863725f297380c86d1293
BaeckerAnna <baecker.anna2@gmail.com>
2022-07-11 15:23:38 +0200

    Funktionen Teil 1 und Teil 2

--- script_01.js
+++ script_01.js
@@ -2,3 +2,37 @@
 
 /***** Funktionen 01 *****/
 
+//1. Kapselung voncodeblöcken
+
+// test();
+
+//Funktionsaufruf (body) | callee
+function test()
+{
+    console.log("Hallo Robert!");
+}
+
+/****** Funktionen 02a ******/
+// 2a. Parametrisierung + Datenüberabe von INNEN
+
+// ausgabeNamen();
+
+function ausgabeNamen() {
+    let firstName = "Anna"; // what happens in Vagas, stays in vegas.
+
+    console.log("Hallo " + firstName + "!");
+}
+
+// console.log(firstName); // Fehler: Scope!
+
+
+/****** Funktionen 02a ******/
+// 2b. Parametrisierung + Datenüberabe von AUSSEN
+
+ausgabeNamenParam("Anna"); // Argument
+ausgabeNamenParam("Craig");
+ausgabeNamenParam("Fatih");
+
+function ausgabeNamenParam(firstName) { // Parameter
+    console.log("Hallo " + firstName + "!");
+}
